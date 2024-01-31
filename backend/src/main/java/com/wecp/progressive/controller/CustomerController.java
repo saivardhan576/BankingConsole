235f8c983a3b734ca00ef043ce3de569824445fe
username <wecp-dummy@gmail.com>
2024-01-31 03:41:21 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/controller/CustomerController.java
+++ backend/src/main/java/com/wecp/progressive/controller/CustomerController.java
@@ -2,34 +2,92 @@ package com.wecp.progressive.controller;
 
 
 import com.wecp.progressive.entity.Customers;
-import com.wecp.progressive.entity.Transactions;
+import com.wecp.progressive.service.CustomerService;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
+@RestController
 public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
 
-    public ResponseEntity<List<Customers>> getAllCustomers() {
-        return null;
-    }
-
-    public ResponseEntity<Customers> getCustomerById(int customerId) {
-        return null;
-    }
-
-    public ResponseEntity<Integer> addCustomer(Customers customers) {
-        return null;
-    }
-
-    public ResponseEntity<Void> updateCustomer(int customerId, Customers customers) {
-        return null;
-    }
-    public ResponseEntity<Void> deleteCustomer(int customerId) {
-        return null;
+    @GetMapping("/customers")
+    public ResponseEntity<List<Customers>>  getAllCustomers()
+    {
+        try {
+            List<Customers> list = customerService.getAllCustomers();
+            return new ResponseEntity<>(list,HttpStatus.OK);
+        } catch (SQLException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    public ResponseEntity<List<Transactions>> getAllTransactionsByCustomerId(int cutomerId) {
-        return null;
+    @GetMapping("/customers/{customerID}")
+    public ResponseEntity<Customers> getCustomerById(@PathVariable int customerID)
+    {
+        try {
+            Customers customers = customerService.getCustomerById(customerID);
+            return new ResponseEntity<>(customers,HttpStatus.OK);
+        } catch (SQLException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
+
+    @PostMapping("/customers")
+    public ResponseEntity<Integer> addCustomer(@RequestBody Customers customers)
+    {
+        try {
+            Integer id = customerService.addCustomer(customers);
+            return new ResponseEntity<>(id,HttpStatus.OK);
+        } catch (SQLException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping("/customers/{customerID}")
+    public void updateCustomer(@PathVariable int customerID,@RequestBody Customers updatedCustomers)
+    {
+        try {
+            customerService.updateCustomer(updatedCustomers);
+        } catch (SQLException e) {
+        }
+    }
+
+    @DeleteMapping("/customers/{customerID}")
+    public  void deleteCustomers(@PathVariable int customerID)
+    {
+        try {
+            customerService.deleteCustomer(customerID);
+        } catch (SQLException e) {
+        }
+    }
+
+    @GetMapping("/customers/fromArrayList")
+    public ResponseEntity<List<Customers>> getAllCustomersFromArrayList()
+    {
+        List<Customers> list = customerService.getAllCustomersFromArrayList();
+        return new ResponseEntity<>(list,HttpStatus.OK);
+    }
+
+    @PostMapping("/customers/toArrayList")
+    public ResponseEntity<List<Customers>> addCustomersToArrayList(@RequestBody Customers customers)
+    {
+        List<Customers> list = customerService.addCustomersToArrayList(customers);
+        return new ResponseEntity<>(list,HttpStatus.OK);
+    }
+
+    @GetMapping("/customers/fromArrayList/{customerId}")
+    public ResponseEntity<List<Customers>> getAllCustomersSortedByNameFromArrayList()
+    {
+        List<Customers> list = customerService.getAllCustomersSortedByNameFromArrayList();
+        return new ResponseEntity<>(list,HttpStatus.OK);
+    }
+
 }
--- backend/src/main/java/com/wecp/progressive/service/CustomerServiceImpl.java
+++ backend/src/main/java/com/wecp/progressive/service/CustomerServiceImpl.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+
 public class CustomerServiceImpl implements CustomerService {
 
     CustomerDAO customerDAO;
