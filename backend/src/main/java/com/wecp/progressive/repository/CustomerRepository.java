0dbf5604b0bfb33114c1b2615ce68b447aa4500c
username <wecp-dummy@gmail.com>
2024-02-11 17:45:50 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/repository/CustomerRepository.java
+++ backend/src/main/java/com/wecp/progressive/repository/CustomerRepository.java
@@ -1,5 +1,4 @@
 package com.wecp.progressive.repository;
-
 import com.wecp.progressive.entity.Customers;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
@@ -13,5 +12,7 @@ public interface CustomerRepository extends JpaRepository<Customers, Integer> {
     Customers findByCustomerId(@Param("customerId") int customerId);
 
     void deleteByCustomerId(@Param("customerId") int customerId);
+
     Customers findByNameAndEmail(@Param("name") String name, @Param("email") String email);
+    Customers findByUsername(String username);
 }
